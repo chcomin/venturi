0a5bd22eed6aee7d75ddf976c07fc5a810409596
Cesar Henrique Comin <chcomin@gmail.com>
2026-02-04 09:37:42 -0300

    Test run

--- examples/image_segmentation/scripts/model.py
+++ examples/image_segmentation/scripts/model.py
@@ -5,7 +5,7 @@ from torch import nn
 from venturi import Config
 
 
-def stage(in_channels: int, out_channels: int) -> nn.Sequential:
+def conv_block(in_channels: int, out_channels: int) -> nn.Sequential:
     """Creates a simple stage with Conv2d, BatchNorm2d, and ReLU layers."""
 
     return nn.Sequential(
@@ -14,19 +14,35 @@ def stage(in_channels: int, out_channels: int) -> nn.Sequential:
         nn.ReLU(),
     )
 
+class SimpleCNN(nn.Module):
+    """A simple CNN model for image segmentation."""
+
+    def __init__(self, in_channels: int, out_channels: int, base_filters: int, num_layers: int):
+        super().__init__()
+
+        layers = []
+
+        layers.append(conv_block(in_channels, base_filters))
+        for _ in range(num_layers):
+            layers.extend([conv_block(base_filters, base_filters)])
+        layers.append(conv_block(base_filters, out_channels))
+
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
+
 
 def get_model(vcfg: Config) -> nn.Module:
     """Creates a simple CNN model based on the dataset parameters in vcfg."""
 
     vcfg_p = vcfg.model.params
 
-    layers = []
-
-    layers.append(stage(vcfg_p.num_input_channels, vcfg_p.base_filters))
-    for _ in range(vcfg_p.num_layers):
-        layers.extend([stage(vcfg_p.base_filters, vcfg_p.base_filters)])
-    layers.append(stage(vcfg_p.base_filters, vcfg_p.num_output_channels))
-
-    model = nn.Sequential(*layers)
+    model = SimpleCNN(
+        vcfg_p.num_input_channels,
+        vcfg_p.num_output_channels,
+        vcfg_p.base_filters,
+        vcfg_p.num_layers,
+    )
 
     return model
