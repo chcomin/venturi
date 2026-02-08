8d04a0910afed43a7ae0ae9167817252b0394d06
Cesar Henrique Comin <chcomin@gmail.com>
2026-02-08 08:23:04 -0300

    bumped version

--- venturi/__init__.py
+++ venturi/__init__.py
@@ -3,7 +3,7 @@ from typing import Any
 from venturi.config import Config, instantiate
 from venturi.core import DataModule, Experiment, TrainingModule
 
-__version__ = "0.9.2"
+__version__ = "0.9.3"
 
 __all__ = [
     "Config",
